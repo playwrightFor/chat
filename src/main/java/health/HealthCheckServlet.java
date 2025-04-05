50e85ececbc23897d7961351fee525ecddad37bf
AlgosStile <oleg.todor@mail.ru>
2025-04-05 07:06:42 +0300

    javadoc

--- src/main/java/health/HealthCheckServlet.java
+++ src/main/java/health/HealthCheckServlet.java
@@ -7,6 +7,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/**
+ * Класс HealthCheckServlet представляет собой сервлет для проверки состояния сервера.
+ * Этот сервлет обрабатывает HTTP GET запросы по пути "/health" и возвращает
+ * информацию о состоянии сервера в формате JSON.
+ * При успешном выполнении запроса сервлет отправляет клиенту ответ с кодом 200 (OK) и
+ * сообщением о том, что сервер работает корректно.
+ */
 @WebServlet("/health")
 public class HealthCheckServlet extends HttpServlet {
     @Override
